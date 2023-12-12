707889fe6ed5de376d130b3922f45685f725b976
Jacob Graver <jacob.graver@gmail.com>
2023-12-12 16:30:48 -0700

    Start of slot machine project

---
+++ Little projects/Slot Machine/Main.py
@@ -0,0 +1,14 @@
+def deposit():
+    while True:
+        amount = input("What is the amount that you would like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Enter a positive number.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+deposit()
