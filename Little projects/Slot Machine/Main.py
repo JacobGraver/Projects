5a97665c4d82df132080d4b04a8850ba481fa07d
Jacob Graver <jacob.graver@gmail.com>
2023-12-13 17:19:44 -0700

    commenting

--- Little projects/Slot Machine/Main.py
+++ Little projects/Slot Machine/Main.py
@@ -1,13 +1,20 @@
+# collect deposit // create function
 def deposit():
+    # while loop
     while True:
         amount = input("What is the amount that you would like to deposit? $")
+        # checking that what they input is a number
         if amount.isdigit():
+            # changing from a string to a number
             amount = int(amount)
             if amount > 0:
+                # if the amount is greater than 0 it will leave the loop
                 break
             else:
+                # if it is not greater than 0
                 print("Enter a positive number.")
         else:
+            # if it is not a number
             print("Please enter a number.")
     return amount
 
