8c46cbc5b9807fd10c80c047023878094577ad62
Jacob Graver <jacob.graver@gmail.com>
2023-12-22 10:37:45 -0700

    Finished functionality

--- Little projects/YouTubeDownloader/main.py
+++ Little projects/YouTubeDownloader/main.py
@@ -3,11 +3,12 @@ from pytube import YouTube
 import tkinter
 import customtkinter
 
+
 # Download function
 def startDownload():
     try:
         ytLink = link.get()
-        ytObject = YouTube(ytLink)
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         video = ytObject.streams.get_highest_resolution()
 
         title.configure(text=ytObject.title, text_color="white")
@@ -16,6 +17,19 @@ def startDownload():
         finishLabel.configure(text="Downloaded!")
     except:
         finishLabel.configure(text="Download error", text_color="red")
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text=per + '%')
+    pPercentage.update()
+
+    # update progress progress
+    progressBar.set(float(percentage_of_completion) / 100)
+
 
 # System settings
 customtkinter.set_appearance_mode("System")
@@ -32,7 +46,7 @@ title.pack(padx=10, pady=10)
 
 # Link input
 url_var = tkinter.StringVar()
-link = customtkinter.CTkEntry(app, width=400, height=40, textvariable=url_var) # origionally 350
+link = customtkinter.CTkEntry(app, width=400, height=40, textvariable=url_var)  # origionally 350
 link.pack()
 
 # Finished Downloading
@@ -46,7 +60,7 @@ pPercentage.pack()
 progressBar = customtkinter.CTkProgressBar(app, width=400)
 progressBar.set(0)
 progressBar.pack(padx=10, pady=10)
-    
+
 # Download button
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
 download.pack(padx=10, pady=10)
