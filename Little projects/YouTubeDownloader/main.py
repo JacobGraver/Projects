5c4d94e4b92629f0776b928c50f3f8667a5c76bc
Jacob Graver <jacob.graver@gmail.com>
2023-12-22 09:41:52 -0700

    adding ui elements

--- Little projects/YouTubeDownloader/main.py
+++ Little projects/YouTubeDownloader/main.py
@@ -12,4 +12,12 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("YouTube Downloader")
 
+# Adding UI elements
+title = customtkinter.CTkLabel(app, text="Insert a YouTube link")
+title.pack(padx=10, pady=10)
+
+# Link input
+link = customtkinter.CTkEntry(app, width=400, height=40) # origionally 350
+link.pack()
+
 app.mainloop()
