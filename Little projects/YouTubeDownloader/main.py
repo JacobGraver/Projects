8d26292e00ee106d16fdf5f10f00a5dd959e14fe
Jacob Graver <jacob.graver@gmail.com>
2023-12-22 10:23:51 -0700

    Creating progress bar

--- Little projects/YouTubeDownloader/main.py
+++ Little projects/YouTubeDownloader/main.py
@@ -9,8 +9,13 @@ def startDownload():
         ytLink = link.get()
         ytObject = YouTube(ytLink)
         video = ytObject.streams.get_highest_resolution()
+
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+        video.download()
+        finishLabel.configure(text="Downloaded!")
     except:
-        print("YouTube link is invalid.")
+        finishLabel.configure(text="Download error", text_color="red")
 
 # System settings
 customtkinter.set_appearance_mode("System")
@@ -30,7 +35,20 @@ url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=400, height=40, textvariable=url_var) # origionally 350
 link.pack()
 
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress percentage
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+    
 # Download button
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack(padx=10, pady=10)
 
 app.mainloop()
