dd1638cf1f04caf3b36b8fbeaabfd868c32aa461
Jacob Graver <jacob.graver@gmail.com>
2023-12-22 09:31:56 -0700

    YouTube Downloader GUI

---
+++ Little projects/YouTubeDownloader/main.py
@@ -0,0 +1,15 @@
+import pytube
+
+import tkinter
+import customtkinter
+
+# System settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("YouTube Downloader")
+
+app.mainloop()
